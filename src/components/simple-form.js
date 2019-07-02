7e9c51afce78a51752539a1acb786c4abf1346a8
Scott Bowler <scott.bowler@learningfuze.com>
2019-07-01 18:03:06 -0700

    Added custom input component

--- src/components/simple-form.js
+++ src/components/simple-form.js
@@ -1,56 +1,30 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import Input from './input';
 
 class SimpleForm extends Component {
     handleFormSubmit(formValues){
-        // When your submit handler is called by redux form
-        // you are passed all the form values
         console.log('On Submit Simple Form Values:', formValues);
     }
 
     resetForm(){
-        // Reset comes from the reduxForm Higher Order Component
         this.props.reset();
     }
 
     render(){
-        // handle submit comes from the reduxForm Higher Order Component
         const { handleSubmit } = this.props;
 
         return (
             <div>
-                <h2 className="center">Simple Form</h2>
-                {/* 
-                    Here we pass what function we want to be called
-                    when the form is submitted as a callback to
-                    handleSubmit. handleSubmit comes from reduxForm
-                */}
+                <h2 className="center">Custom Form</h2>
                 <form onSubmit={handleSubmit(this.handleFormSubmit)}>
-                    <div className="input-field">
-                        <label>Name</label>
-                        {/* 
-                            Field only requires name and component
-                            name is used to identify the input and must 
-                            be unique in a given form
-                            component is what element to render
-                        
-                            In the next version we will pass a custom
-                            component into field via component
-                        */}
-                        <Field name="name" component="input" />
-                    </div>
-                    <div className="input-field">
-                        <label>email</label>
-                        <Field name="email" component="input" autoComplete="email" />
-                    </div>
-                    <div className="input-field">
-                        <label>Password</label>
-                        <Field name="password" component="input" type="password" autoComplete="new-password" />
-                    </div>
-                    <div className="input-field">
-                        <label>Confirm Password</label>
-                        <Field name="confirmPassword" component="input" type="password" autoComplete="new-password" />
-                    </div>
+                    <Field label="Name" name="name" component={Input} />
+
+                    <Field label="Email" name="email" component={Input} autoComplete="email" />
+
+                    <Field label="Password" name="password" component={Input} type="password" autoComplete="new-password" />
+
+                    <Field label="Confirm Password" name="confirmPassword" component={Input} type="password" autoComplete="new-password" />
 
                     <div className="form-actions">
                         <button onClick={this.resetForm.bind(this)} type="button">Reset Form</button>
@@ -62,21 +36,6 @@ class SimpleForm extends Component {
     }
 }
 
-/*
-    To use redux form we must wrap the component
-    with the reduxForm higher order component
-
-    reduxForm takes a configuration object
-    the only required option in the configuration
-    object is the 'form' key. This sets the name
-    of the form. This is where we are giving the form
-    a unique identifier for redux form to use to refer to
-    this form.
-
-    reduxForm returns a function that we immediately call
-    and pass it our component. This adds the redux-form
-    functionality to our component
-*/
 export default reduxForm({
     form: 'simple-form'
 })(SimpleForm);
